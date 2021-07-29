1ad39188ed27ed58453a069b0d5453ac7a6e1ccf
Tiago Barbosa <tiago@farias.cc>
2021-07-29 15:58:14 -0300

    CRUD Departments

--- WebSales/Startup.cs
+++ WebSales/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSales.Data;
 
 namespace WebSales
 {
@@ -33,6 +35,9 @@ namespace WebSales
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<WebSalesContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebSalesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
