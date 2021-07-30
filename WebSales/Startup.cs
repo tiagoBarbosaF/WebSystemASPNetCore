c94eba9920e02a7633e3ec3d9676c61d510fe43d
Tiago Barbosa <tiago@farias.cc>
2021-07-30 11:51:22 -0300

    Docker SQLServer created and first migration

--- WebSales/Startup.cs
+++ WebSales/Startup.cs
@@ -37,7 +37,7 @@ namespace WebSales
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<WebSalesContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("WebSalesContext")));
+                    options.UseSqlServer(Configuration.GetConnectionString("WebSalesContext"), builder => builder.MigrationsAssembly("WebSales")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
