5bdd0c229837d61521b545f3c29c24cf1b523cdc
Tiago Barbosa <tiago@farias.cc>
2021-07-30 17:13:28 -0300

    Simple Create form

--- WebSales/Controllers/SellersController.cs
+++ WebSales/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebSales.Models;
 using WebSales.Services;
 
 namespace WebSales.Controllers
@@ -20,6 +21,19 @@ namespace WebSales.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
