36c37be4cbaaf3a1581aa6e98eba5aaa249e73cd
Tiago Barbosa <tiago@farias.cc>
2021-08-03 10:48:00 -0300

    Exception handling for delete

--- WebSales/Controllers/SellersController.cs
+++ WebSales/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using WebSales.Models;
 using WebSales.Models.ViewModels;
 using WebSales.Services;
+using WebSales.Services.Exceptions;
 
 namespace WebSales.Controllers
 {
@@ -67,8 +68,15 @@ namespace WebSales.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch(IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
--- WebSales/Services/SellerService.cs
+++ WebSales/Services/SellerService.cs
@@ -35,9 +35,16 @@ namespace WebSales.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales.");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
@@ -52,7 +59,7 @@ namespace WebSales.Services
                 _context.Update(obj);
                 await _context.SaveChangesAsync();
             }
-            catch(DbConcurrencyException e)
+            catch (DbConcurrencyException e)
             {
                 throw new DbConcurrencyException(e.Message);
             }
